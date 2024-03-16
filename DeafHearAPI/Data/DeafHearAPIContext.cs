4cce80d53107e96683dc1a6fb0f815600bdb467b
berkovics <berkovicsgellert@ktch.hu>
2024-03-16 15:23:04 +0100

    first commit

---
+++ DeafHearAPI/Data/DeafHearAPIContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DeafHearAPI.Models;
+
+namespace DeafHearAPI.Data
+{
+    public class DeafHearAPIContext : DbContext
+    {
+        public DeafHearAPIContext (DbContextOptions<DeafHearAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DeafHearAPI.Models.User> User { get; set; } = default!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\mssqllocaldb;Database=DeafHearAPIContext-1be11bae-0a30-41de-b30c-02aecf581ea6;Trusted_Connection=True;MultipleActiveResultSets=true");
+        }
+    }
+}
---
+++ DeafHearAPI/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DeafHearAPI.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Tajszam { get; set; }
+        public string Lakcim { get; set; }
+        public string Email { get; set; }
+    }
+}
